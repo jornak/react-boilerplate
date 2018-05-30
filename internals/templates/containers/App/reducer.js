e72c4b5a82f6ff2744fdc74cec592468ab7f8912
jornakdev <31213225+jornakdev@users.noreply.github.com>
2018-05-30 13:14:12 +0200

    fix lint errors

--- internals/templates/containers/App/reducer.js
+++ internals/templates/containers/App/reducer.js
@@ -14,7 +14,7 @@ import { fromJS } from 'immutable';
 
 // The initial state of the App
 const initialState = fromJS({
- });
+});
 
 function appReducer(state = initialState, action) {
   switch (action.type) {
